3ae5d5a2b6eb418fc8c776b28afcfd87d8fbe738
opilane <opilane@HP-CZC6297B8P>
2022-02-22 11:02:27 +0200

    Add project files.

---
+++ Models/Guest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Kutse.Models
+{
+    public class Guest
+    {
+        [Required(ErrorMessage = "Sissesta nimi")]
+        public string name { get; set; }
+        [Required(ErrorMessage = "Sissesta email")]
+        [RegularExpression(@"\.+\@.+\..+", ErrorMessage = "Vali sisestatud email")]
+        public string email { get; set; }
+        [Required(ErrorMessage = "Sissesta telefoni number")]
+        [RegularExpression(@"\+372",ErrorMessage ="Numbril alguses peal olema +372")]
+        public string phone { get; set; }
+        [Required(ErrorMessage = "Sissesta oma valik")]
+        public string willattend { get; set; }
+
+    }
+}
